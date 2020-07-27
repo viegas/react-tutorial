8751203352a297500fa5b43d5ba1c25bc14271c1
viegas <felipesviegas@gmail.com>
2020-07-27 15:15:41 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ({ titulo }) => {
+    const [val, setVal] = useState(0);
+
+    useEffect(() => {
+        console.log('Mountou o functional component');
+    }, []);
+
+    const fn = () => setVal(val + 1);
+
+    return (
+        <div className="App">
+            <span
+                style={{
+                    color: 'blue',
+                    backgroundColor: 'yellow',
+                }}
+            >
+                {' '}
+                Meu todo {titulo}{' '}
+            </span>
+            <span> Meu todo {val} </span>
+            <button onClick={fn}>btn</button>
+        </div>
+    );
+};
+
+// -----------------------------------
+
+export class App2 extends Component {
+    state = {
+        test2: 0,
+    };
+
+    componentDidMount() {
+        console.log(' Componente montou ');
+    }
+
+    exemploFn = (param) => {
+        const { test2 } = this.state;
+        this.setState({
+            test2: test2 + 1,
+        });
+    };
+
+    render() {
+        const { test } = this.props;
+        const { test2 } = this.state;
+
+        return (
+            <div className="App">
+                <span> Meu todo {test}</span>
+                <span> Meu todo {test2}</span>
+                <button onClick={this.exemploFn}>btn</button>
+            </div>
+        );
+    }
 }
 
 export default App;
